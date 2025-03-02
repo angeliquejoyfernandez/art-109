0f025e20cbd66659bf5e0bf0dbe7750d3ac98e6a
angeliquejoyfernandez <angeliquejoy.fernandez@sjsu.edu>
2025-03-02 02:01:43 -0800

    bark bark bark

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,40 @@
+// console.log("hello hello");
+
+
+// let pageTitle = document.querySelector("#page-title")
+
+// //javascript timeout changes h1 title after 3secs
+// setTimeout(function(){
+//     pageTitle.style.color = "pink"
+//     // document.querySelector("#page-title").style.color ="pink";
+// },3000)
+
+// //click event changes header background color
+// document.querySelector("header").onclick = function() {
+//     // console.log("clicked header");
+//     document.querySelector("body").style.backgroundColor = "black";
+
+//     }
+
+// document.querySelector("#image-0").addEventListener("click", function(){
+//    document.querySelector("#image-0").style.visibility = "hidden";
+// })
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-0").style.visibility = "hidden";
+    alert("where did kirby go");
+ })
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "hidden";
+ })
+ document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden";
+ })
+ document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "hidden";
+ })
+ document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "hidden";
+ })
+ document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "hidden";
+ })
